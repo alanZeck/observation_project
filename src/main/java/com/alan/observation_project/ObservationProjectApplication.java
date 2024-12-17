41f8e7a966d69e10c93417ba82d6b9093235a1f8
Alan <alanbellec.dev@gmail.com>
2024-12-17 23:45:54 +0100

    init

---
+++ src/main/java/com/alan/observation_project/ObservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alan.observation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alan/observation_project/ObservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.observation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
